0cd73ec29b880e7928a1bb90ac3bc45a80381002
zachary62 <zhuang333@wisc.edu>
2025-01-09 02:54:03 +0000

    rename the repo and pypi

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="minillmflow",
-    version="0.0.5",
+    name="packetflow",
+    version="0.0.0",
     packages=find_packages(),
     author="Zachary Huang",
     author_email="zh2408@columbia.edu",
-    description="A Minimalist Framework for LLM Workflows in 100 Lines of Code",
-    url="https://github.com/zachary62/miniLLMFlow",
+    description="Minimalist LLM Framework in 100 Lines. Enable LLMs to Program Themselves.",
+    url="https://github.com/miniLLMFlow/PocketFlow/",
 )
