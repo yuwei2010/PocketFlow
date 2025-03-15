f8ba9cc877581473d887421851d2ff65b447e5a1
zachary62 <zhuang333@wisc.edu>
2025-03-14 21:57:50 -0400

    update steup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pocketflow",
+    version="0.0.1",
+    packages=find_packages(),
+    author="Zachary Huang",
+    author_email="zh2408@columbia.edu",
+    description="Minimalist LLM Framework in 100 Lines. Enable LLMs to Program Themselves.",
+    url="https://github.com/miniLLMFlow/PocketFlow/",
+)
