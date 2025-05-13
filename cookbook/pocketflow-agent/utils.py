458d2bdc77114774b725778e0e0d0d8e347e22fe
mingupup <2539823901@qq.com>
2025-05-13 11:31:44 +0800

    pocketflow-agent add search_web_brave

--- cookbook/pocketflow-agent/utils.py
+++ cookbook/pocketflow-agent/utils.py
@@ -1,6 +1,7 @@
 from openai import OpenAI
 import os
 from duckduckgo_search import DDGS
+import requests
 
 def call_llm(prompt):    
     client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY", "your-api-key"))
@@ -10,10 +11,31 @@ def call_llm(prompt):
     )
     return r.choices[0].message.content
 
-def search_web(query):
+def search_web_duckduckgo(query):
     results = DDGS().text(query, max_results=5)
     # Convert results to a string
     results_str = "\n\n".join([f"Title: {r['title']}\nURL: {r['href']}\nSnippet: {r['body']}" for r in results])
+    return results_str
+
+def search_web_brave(query):
+
+    url = f"https://api.search.brave.com/res/v1/web/search?q={query}"
+    api_key = "your brave search api key"
+
+    headers = {
+        "accept": "application/json",
+        "Accept-Encoding": "gzip",
+        "x-subscription-token": api_key
+    }
+
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        results = data['web']['results']
+        results_str = "\n\n".join([f"Title: {r['title']}\nURL: {r['url']}\nDescription: {r['description']}" for r in results])     
+    else:
+        print(f"Request failed with status code: {response.status_code}")
     return results_str
     
 if __name__ == "__main__":
